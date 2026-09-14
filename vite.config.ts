4795617b2728aaa6dd69322c4a3f3f5bfa6c0c70
Liza Ruban <yelyzaveta.ruban2023@gmail.com>
2026-09-14 21:16:11 +0300

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/react-v3-blended-starter/",
+  base: "./",
   plugins: [react()],
   build: {
     sourcemap: true,
